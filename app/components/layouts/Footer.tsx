7a425725606792d59d11172e575a6307ad3f47d3
75ks <kenta.s705@gmail.com>
2023-05-18 23:44:36 +0900

    共通のヘッダー・フッターを作成

---
+++ app/components/layouts/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-emerald-200 p-4 text-center absolute bottom-0 w-full">
+      <p>©2023 sekinekenta</p>
+    </footer>
+  )
+}
---
+++ app/components/layouts/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="bg-emerald-200 p-4 text-center w-full">
+      <p>Study-React</p>
+    </header>
+  )
+}
