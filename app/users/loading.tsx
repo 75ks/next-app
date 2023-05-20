40d2aabd8d6855152cd2d5ebd14ff8ea3921d2bd
75ks <kenta.s705@gmail.com>
2023-05-20 18:34:45 +0900

    ローディングを追加

---
+++ app/users/loading.tsx
@@ -0,0 +1,4 @@
+export default function Loading() {
+  return <p className="p-2 text-center">Loading...</p>;
+  
+}
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -2,15 +2,18 @@
 
 import User from "@/pages/types/user";
 import { useEffect, useState } from "react";
+import Loading from "./loading";
 
 export default function Users() {
   const [users, setUsers] = useState<User[]>([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const getData = async () => {
       const res = await fetch("http://localhost:3000/api/users");
       const data = await res.json();
       setUsers(data);
+      setLoading(false);
     }
     getData();
     return () => {
@@ -22,13 +25,17 @@ export default function Users() {
       <div className="text-center mb-4">
         <p>■ ユーザーリスト</p>
       </div>
-      {users.map((user, index) => (
-        <ul key={index} className="text-center p-2 border-t">
-          <li>ID：{user.id}</li>
-          <li>名前：{user.name}</li>
-          <li>年齢：{user.age}</li>
-        </ul>
-      ))}
+      {loading ? (
+        <Loading />
+      ) : (
+        users.map((user, index) => (
+          <ul key={index} className="text-center p-2 border-t">
+            <li>ID：{user.id}</li>
+            <li>名前：{user.name}</li>
+            <li>年齢：{user.age}</li>
+          </ul>
+        ))
+      )}
     </div>
-  )
+  );
 }
