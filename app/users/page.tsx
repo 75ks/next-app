23e381aa848355a21cb8c351d276e9c4500d366c
75ks <kenta.s705@gmail.com>
2023-05-20 17:24:36 +0900

    APIコールをし、取得した情報を表示するページを作成

---
+++ app/users/page.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import User from "@/pages/types/user";
+import { useEffect, useState } from "react";
+
+
+export default function Users() {
+  const [users, setUsers] = useState<User[]>([]);
+
+  const getData = async () => {
+    const res = await fetch("http://localhost:3000/api/users");
+    const data = await res.json();
+    setUsers(data);
+  }
+
+  useEffect(() => {
+    getData();
+    return () => {
+    }
+  }, []);
+
+  return (
+    <div className="p-4">
+      <div className="text-center mb-4">
+        <p>■ ユーザーリスト</p>
+      </div>
+      {users.map((user, index) => (
+        <ul key={index} className="text-center p-2 border-t">
+          <li>ID：{user.id}</li>
+          <li>名前：{user.name}</li>
+          <li>年齢：{user.age}</li>
+        </ul>
+      ))}
+    </div>
+  )
+}
