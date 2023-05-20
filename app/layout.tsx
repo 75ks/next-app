2bd5c98ffa8a405a543958d3fa6d6c993519386f
75ks <kenta.s705@gmail.com>
2023-05-20 17:13:37 +0900

    herder、footerを大文字から小文字に変更

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import './globals.css'
-import Footer from './components/layouts/Footer'
-import Header from './components/layouts/Header'
+import Footer from './components/layouts/footer'
+import Header from './components/layouts/header'
 
 export const metadata = {
   title: 'Create Next App',
