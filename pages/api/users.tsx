e34858363e2cdf619e79c6ae4cea65406830849b
75ks <kenta.s705@gmail.com>
2023-05-28 17:27:48 +0900

    ユーザー情報を追加

--- pages/api/users.tsx
+++ pages/api/users.tsx
@@ -5,27 +5,52 @@ const users: User[] = [
   {
     id: 1,
     name: "Taro",
-    age: 18
+    age: 18,
+    phoneNumber: "09012345678",
+    postCode: "1234567",
+    address1: "Tokyo",
+    address2: "AAAAA",
+    address3: "AAA"
   },
   {
     id: 2,
     name: "Jiro",
-    age: 16
+    age: 16,
+    phoneNumber: "09023456789",
+    postCode: "2345678",
+    address1: "Kanagawa",
+    address2: "BBBBB",
+    address3: "BBB"
   },
   {
     id: 3,
     name: "Saburo",
-    age: 20
+    age: 20,
+    phoneNumber: "09034567890",
+    postCode: "3456789",
+    address1: "Saitqma",
+    address2: "CCCCC",
+    address3: "CCC"
   },
   {
     id: 4,
     name: "Siro",
-    age: 24
+    age: 24,
+    phoneNumber: "09045678901",
+    postCode: "4567890",
+    address1: "Tchiba",
+    address2: "DDDDD",
+    address3: "DDD"
   },
   {
     id: 5,
     name: "Goro",
-    age: 15
+    age: 15,
+    phoneNumber: "09056789012",
+    postCode: "5678901",
+    address1: "Ibaraki",
+    address2: "EEEEE",
+    address3: "EEE"
   },
 ];
 
--- pages/types/user.tsx
+++ pages/types/user.tsx
@@ -2,4 +2,9 @@ export default interface User {
   id: number;
   name: string;
   age: number;
+  phoneNumber: string;
+  postCode: string;
+  address1: string;
+  address2: string;
+  address3: string;
 }
